f4064dc65ee7753b2ffbf4b5159b40f92063b4b2
Huy <huy2007mu2@gmail.com>
2026-09-15 15:54:59 +0700

    xong lap 1

---
+++ jsnc_fa26-main/lap1.js
@@ -0,0 +1,8 @@
+let name = "nguyễn Kim huy";
+const age = 20;
+let address = "Hà Nội";
+let isStudent = true;
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Address:", address);
+console.log("Is Student:", isStudent);
